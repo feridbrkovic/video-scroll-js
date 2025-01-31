a20a38c795f34216aef6c9f9c28baa01054658e1
Ferid Brkovic <fbrkovic@gmail.com>
2025-01-31 10:07:34 +0100

    changes

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
-const canvas2 = document.getElementById("head-bob-turn");
-const context2 = canvas2.getContext("2d");
-canvas2.width = window.innerWidth;
-canvas2.height = window.innerHeight;
+const canvas = document.getElementById("moving-car");
+const context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const images2 = [];
-const frameCount2 = 131;
+const images = [];
+const frameCount = 232;
 let currentFrameIndex = 0;
 
 // Generate frame URLs
-const currentFrame2 = (index) =>
+const currentFrame = (index) =>
     `frames/${(index + 1).toString().padStart(0, "0")}.avif`;
 
 // Preload images
-for (let i = 0; i < frameCount2; i++) {
+for (let i = 0; i < frameCount; i++) {
     const img = new Image();
-    img.src = currentFrame2(i);
-    images2.push(img);
+    img.src = currentFrame(i);
+    images.push(img);
 }
 
 // Initial render
-images2[0].onload = render2;
+images[0].onload = render;
 
 function handleScroll() {
     const scrollTop = window.scrollY;
@@ -34,16 +34,16 @@ function handleScroll() {
     );
 
     if (progress > 0 && progress <= 1) {
-        currentFrameIndex = Math.floor(progress * (frameCount2 - 1));
-        render2();
+        currentFrameIndex = Math.floor(progress * (frameCount - 1));
+        render();
     }
 }
 
-function render2() {
-    context2.clearRect(0, 0, canvas2.width, canvas2.height);
-    const img = images2[currentFrameIndex];
+function render() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    const img = images[currentFrameIndex];
     if (img.complete) {
-        context2.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
     }
 }
 
@@ -55,13 +55,13 @@ function resizeCanvas() {
     const windowAspectRatio = window.innerWidth / window.innerHeight;
 
     if (windowAspectRatio > aspectRatio) {
-        canvas2.width = window.innerWidth;
-        canvas2.height = window.innerWidth / aspectRatio;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerWidth / aspectRatio;
     } else {
-        canvas2.height = window.innerHeight;
-        canvas2.width = window.innerHeight * aspectRatio;
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerHeight * aspectRatio;
     }
-    render2();
+    render();
 }
 
 // Attach resize function
